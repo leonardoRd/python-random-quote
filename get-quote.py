6039792d18cee9fd605f762229457c209099a089
Leo <gaitanleonardo6@gmail.com>
2021-09-04 18:30:10 -0300

    Add how to insert lines in a file and more optional things

--- get-quote.py
+++ get-quote.py
@@ -3,14 +3,20 @@ import random
 def primario():
   #  print("Keep it logically awesome.")
 
-
-  f = open("quotes.txt")
-  quotes = f.readlines()
+  f = open("quotes.txt", 'a')
+  f.write("Introduction new line in quotes.txt, add other\r\n")
   f.close()
+
+  t = open("quotes.txt")
+  quotes = t.readlines()
+  t.close()
+  
   last = len(quotes) - 1
   rnd = random.randint(0, last)
+  rndTwo = random.randint(0, last)
 
   print(quotes[rnd])
+  print(quotes[rndTwo])
 
 if __name__== "__main__":
   primario()
